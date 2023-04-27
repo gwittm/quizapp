745dafa19e2ecdc4804fcb214250315b49629599
Gabriele Wittmann <gabrielewittmann@web.de>
2023-04-27 11:05:07 +0200

    toggle Bookmark

--- js/index.js
+++ js/index.js
@@ -1,6 +1,5 @@
 /* const bodyElement = document.querySelector('[data-js="body"]');
 
-
 const toggleMode = document.querySelector('[data-js="toggle__button"]');
 toggleMode.addEventListener("click", () => {
   bodyElement.classList.toggle("dark");
@@ -8,8 +7,8 @@ toggleMode.addEventListener("click", () => {
 
 
 
+//Answer-Boxes
 const answerBox1 = document.querySelector('[data-js="card__answer__toggle1"]');
-
 const toggleModeAnswer1 = document.querySelector('[data-js="card__answer__toggle__button1"]');
 toggleModeAnswer1.addEventListener("click", () => {
   answerBox1.classList.toggle("visible");
@@ -17,7 +16,6 @@ toggleModeAnswer1.addEventListener("click", () => {
 
 
 const answerBox2 = document.querySelector('[data-js="card__answer__toggle2"]');
-
 const toggleModeAnswer2 = document.querySelector('[data-js="card__answer__toggle__button2"]');
 toggleModeAnswer2.addEventListener("click", () => {
   answerBox2.classList.toggle("visible");
@@ -25,8 +23,21 @@ toggleModeAnswer2.addEventListener("click", () => {
 
 
 const answerBox3 = document.querySelector('[data-js="card__answer__toggle3"]');
-
 const toggleModeAnswer3 = document.querySelector('[data-js="card__answer__toggle__button3"]');
 toggleModeAnswer3.addEventListener("click", () => {
   answerBox3.classList.toggle("visible");
 });
+
+
+
+//Bookmarks
+const bookmarkButton =document.querySelector('[data-js="card__bookmark_1"]');
+const bookmarkIcon = document.querySelector('[data-js="ribbon1"]');
+
+/* const bookmarkButton2 =document.querySelector('[data-js="card__bookmark_2]');
+const bookmarkIcon2 = document.querySelector('[data-js="ribbon]');
+const bookmarkIconFooter = document.querySelector('[data-js="bookmark-icon-footer"]'); */
+ 
+
+bookmarkButton.addEventListener("click", () => {
+  bookmarkIcon.classList.toggle("choose");});
