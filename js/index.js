0dcc52e3dfa542275f23071eba288ef63bc86d8b
Gabriele Wittmann <gabrielewittmann@web.de>
2023-07-10 21:25:16 +0200

    fix Bookmarks toggle

--- js/index.js
+++ js/index.js
@@ -1,43 +1,56 @@
-/* const bodyElement = document.querySelector('[data-js="body"]');
-
-const toggleMode = document.querySelector('[data-js="toggle__button"]');
-toggleMode.addEventListener("click", () => {
-  bodyElement.classList.toggle("dark");
-}); */
-
-
-
-//Answer-Boxes
+// Home -- Answer-Boxes Toggle
+//AnswerBox 1
 const answerBox1 = document.querySelector('[data-js="card__answer__toggle1"]');
-const toggleModeAnswer1 = document.querySelector('[data-js="card__answer__toggle__button1"]');
-toggleModeAnswer1.addEventListener("click", () => {
-  answerBox1.classList.toggle("visible");
-});
+const toggleModeAnswer1 = document.querySelector(
+  '[data-js="card__answer__toggle__button1"]'
+);
 
+toggleModeAnswer1.addEventListener("click", () =>
+  answerBox1.classList.toggle("visible")
+);
+
+/* if (document.body.classList.contains("dark-theme")) {
+    document.getElementById("theme-btn").innerHTML="Light Mode";
+  } else if (document.body.classList.contains("light-theme")) {
+    document.getElementById("theme-btn").innerHTML="Dark Mode";
+  } */
 
 const answerBox2 = document.querySelector('[data-js="card__answer__toggle2"]');
-const toggleModeAnswer2 = document.querySelector('[data-js="card__answer__toggle__button2"]');
+const toggleModeAnswer2 = document.querySelector(
+  '/* [data-js="card__answer__toggle__button2"]'
+);
+
 toggleModeAnswer2.addEventListener("click", () => {
   answerBox2.classList.toggle("visible");
 });
 
-
 const answerBox3 = document.querySelector('[data-js="card__answer__toggle3"]');
-const toggleModeAnswer3 = document.querySelector('[data-js="card__answer__toggle__button3"]');
+const toggleModeAnswer3 = document.querySelector(
+  '[data-js="card__answer__toggle__button3"]'
+);
+
 toggleModeAnswer3.addEventListener("click", () => {
   answerBox3.classList.toggle("visible");
 });
 
+//Home Bookmarks Toggle
+const bookmarkButton1 = document.querySelector('[data-js="card__bookmark_1"]');
+const bookmarkIcon1 = document.querySelector('[data-js="ribbon1"]');
 
+const bookmarkButton2 = document.querySelector('[data-js="card__bookmark_2"]');
+const bookmarkIcon2 = document.querySelector('[data-js="ribbon2"]');
 
-//Bookmarks
-const bookmarkButton =document.querySelector('[data-js="card__bookmark_1"]');
-const bookmarkIcon = document.querySelector('[data-js="ribbon1"]');
+const bookmarkButton3 = document.querySelector('[data-js="card__bookmark_3"]');
+const bookmarkIcon3 = document.querySelector('[data-js="ribbon3"]');
 
-/* const bookmarkButton2 =document.querySelector('[data-js="card__bookmark_2]');
-const bookmarkIcon2 = document.querySelector('[data-js="ribbon]');
-const bookmarkIconFooter = document.querySelector('[data-js="bookmark-icon-footer"]'); */
- 
+bookmarkButton1.addEventListener("click", () => {
+  bookmarkIcon1.classList.toggle("choose");
+});
 
-bookmarkButton.addEventListener("click", () => {
-  bookmarkIcon.classList.toggle("choose");});
+bookmarkButton2.addEventListener("click", () => {
+  bookmarkIcon2.classList.toggle("choose");
+});
+
+bookmarkButton3.addEventListener("click", () => {
+  bookmarkIcon3.classList.toggle("choose");
+});
